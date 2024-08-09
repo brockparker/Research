93952b95a776ba9ebe0ce5f9438fea893384154f
Brock Parker <baparker@arizona.edu>
2024-08-09 10:39:30 -0700

    Restructure files

--- CCDs/Sophia_QE/plot_sophia.py
+++ CCDs/Sophia_QE/plot_sophia.py
@@ -12,51 +12,61 @@ import pandas as pd
 from astropy.io import fits
 from matplotlib.colors import LogNorm
 
-folder = '/home/GitHub/Research/CCDs/Sophia_QE/'
+folder = '/home/GitHub/Research/CCDs/Sophia_QE/Data/'
 
-dates = ['','','','20240724']
+dates = ['20240718','20240719','20240722','20240724', '20240726']
 
-wl = 300
+#Good, Good, Water, Water warm and cold, Good
 
-prefix = 'warm'
+wls = [245, 290, 295, 300, 300]
 
-science_file = r'D:\Sophia\20240724\QE\test_science_{:.0f}.fits'.format(wl)
-dark_file = r'D:\Sophia\20240724\QE\test_dark_{:.0f}.fits'.format(wl)
-bias_file = r'D:\Sophia\20240724\QE\test_bias_{:.0f}.fits'.format(wl)
+in_folder = ['QE', 'QE', ['QE', 'warm'], ['QE', 'First']]
 
-science = fits.getdata(science_file)[0].astype('int32')
-dark = fits.getdata(dark_file)[0].astype('int32')
-bias = fits.getdata(bias_file)[0].astype('int32')
+vmins = [[1000, 10000, 10000, 6000, 1000]] #24
+vmaxes = [[25000, 60000, 30000, 8000, 30000]] #24
 
-reduced = (science - bias) - (dark - bias)
+for i, date in enumerate(dates):
 
-plt.imshow(bias, vmin = 1000, vmax = 25000, cmap='inferno')
-plt.title('Bias')
-plt.colorbar()
-plt.savefig(r'D:\Sophia\20240724\QE\bias_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
-plt.show()
-
-plt.imshow(science, vmin = 10000, vmax = 60000, cmap='inferno')
-plt.title('Raw Science')
-plt.colorbar()
-plt.savefig(r'D:\Sophia\20240724\QE\science_raw_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
-plt.show()
-
-plt.imshow(dark, vmin = 10000, vmax = 30000, cmap='inferno')
-plt.title('Raw Dark')
-plt.colorbar()
-plt.savefig(r'D:\Sophia\20240724\QE\dark_raw_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
-plt.show()
-
-plt.imshow(dark - bias, vmin = 6000, vmax = 8000, cmap='inferno')
-plt.title('Bias Subtracted Dark')
-plt.colorbar()
-plt.savefig(r'D:\Sophia\20240724\QE\dark_sub_bias_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
-plt.show()
-
-
-plt.imshow(reduced, norm=LogNorm(vmin = 1000, vmax = 30000), cmap='inferno')
-plt.title('Reduced Science')
-plt.colorbar()
-plt.savefig(r'D:\Sophia\20240724\QE\science_reduced_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
-plt.show()
+    for folder in in_folder[i]:
+        science_file = folder + date + '/QE'
+        
+        science_file = r'D:\Sophia\20240724\QE\test_science_{:.0f}.fits'.format(wl)
+        dark_file = r'D:\Sophia\20240724\QE\test_dark_{:.0f}.fits'.format(wl)
+        bias_file = r'D:\Sophia\20240724\QE\test_bias_{:.0f}.fits'.format(wl)
+        
+        science = fits.getdata(science_file)[0].astype('int32')
+        dark = fits.getdata(dark_file)[0].astype('int32')
+        bias = fits.getdata(bias_file)[0].astype('int32')
+        
+        reduced = (science - bias) - (dark - bias)
+        
+        plt.imshow(bias, vmin = 1000, vmax = 25000, cmap='inferno')
+        plt.title('Bias')
+        plt.colorbar()
+        plt.savefig(r'D:\Sophia\20240724\QE\bias_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
+        plt.show()
+        
+        plt.imshow(science, vmin = 10000, vmax = 60000, cmap='inferno')
+        plt.title('Raw Science')
+        plt.colorbar()
+        plt.savefig(r'D:\Sophia\20240724\QE\science_raw_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
+        plt.show()
+        
+        plt.imshow(dark, vmin = 10000, vmax = 30000, cmap='inferno')
+        plt.title('Raw Dark')
+        plt.colorbar()
+        plt.savefig(r'D:\Sophia\20240724\QE\dark_raw_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
+        plt.show()
+        
+        plt.imshow(dark - bias, vmin = 6000, vmax = 8000, cmap='inferno')
+        plt.title('Bias Subtracted Dark')
+        plt.colorbar()
+        plt.savefig(r'D:\Sophia\20240724\QE\dark_sub_bias_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
+        plt.show()
+        
+        
+        plt.imshow(reduced, norm=LogNorm(vmin = 1000, vmax = 30000), cmap='inferno')
+        plt.title('Reduced Science')
+        plt.colorbar()
+        plt.savefig(r'D:\Sophia\20240724\QE\science_reduced_{}_{:.0f}.png'.format(prefix, wl), dpi = 250)
+        plt.show()
