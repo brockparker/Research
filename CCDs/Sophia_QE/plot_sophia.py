c5684633dd5d5596599e23ba67a154616fea400d
Brock Parker <brockaustin09@gmail.com>
2024-09-30 03:01:14 -0700

    update sophia

--- CCDs/Sophia_QE/plot_sophia.py
+++ CCDs/Sophia_QE/plot_sophia.py
@@ -21,9 +21,10 @@ plt.rc('ytick', labelsize=12)
 plt.rc('legend', fontsize=12)
 # BP Stylization parameters.
 
-# base_dir = '/home/GitHub/Research/CCDs/Sophia_QE/Data/'
-base_dir = r'C:\Users\Brock\Documents\Git\Research\CCDs\Sophia_QE\Data'
-save_dir = r'C:\Users\Brock\Documents\Git\Research\CCDs\Sophia_QE\Photos'
+base_dir = r'/home/brockparker/Documents/GitHub/Research/CCDs/Sophia_QE/Data'
+save_dir = r'/home/brockparker/Documents/GitHub/Research/CCDs/Sophia_QE/Photos'
+#base_dir = r'C:\Users\Brock\Documents\Git\Research\CCDs\Sophia_QE\Data'
+#save_dir = r'C:\Users\Brock\Documents\Git\Research\CCDs\Sophia_QE\Photos'
 
 dates = ['20240718','20240719','20240722','20240724', '20240726', '20240906']
 
@@ -41,11 +42,11 @@ temps = [['-85'], ['-85'], ['-85', '15'], ['20', '10'], ['-85'], ['5']]
 for i, date in enumerate(dates):
     wl = wls[i]
     for j, folder in enumerate(in_folder[i]):
-        base_file = base_dir + '\\' + date + '\\' + folder
+        base_file = base_dir + '/' + date + '/' + folder
         
-        science_file = base_file + r'\science_{:.0f}_2.fits'.format(wl)
-        bias_file = base_file + r'\bias_{:.0f}.fits'.format(wl)
-        dark_file = base_file + r'\dark_{:.0f}.fits'.format(wl)
+        science_file = base_file + r'/science_{:.0f}_2.fits'.format(wl)
+        bias_file = base_file + r'/bias_{:.0f}.fits'.format(wl)
+        dark_file = base_file + r'/dark_{:.0f}.fits'.format(wl)
         
         science = fits.getdata(science_file)[0].astype('float32')
         dark = fits.getdata(dark_file)[0].astype('float32')
@@ -59,7 +60,7 @@ for i, date in enumerate(dates):
         
         temp = temps[i][j]
                 
-        fig, ax = plt.subplots(layout='tight', figsize=(5,6))
+        fig, ax = plt.subplots(layout='tight', figsize=(5,4))
 
         image = ax.imshow(reduced, vmin=vmins[i][j], vmax=vmaxes[i][j], cmap='inferno')#norm=LogNorm(vmin=vmins[i][j], vmax=vmaxes[i][j] ))
         
@@ -71,8 +72,8 @@ for i, date in enumerate(dates):
         
         patches = [mlines.Line2D([0], [0], marker='.', markersize=0, label=temp + '$^{{\circ}}$ C', ls='')]
         plt.legend(handles=patches, loc='upper left', handletextpad=-2)                
-        fig.tight_layout(pad=1)
-        plt.savefig(save_dir + r'\Science_{}_{:.0f}_{}.png'.format(date, wl, temp),dpi = 500)
+        fig.tight_layout()
+        plt.savefig(save_dir + r'/Science_{}_{:.0f}_{}.png'.format(date, wl, temp),dpi = 500)
         plt.show()
                         
         #also gaussian blur the bad images to see if they look like the good images
